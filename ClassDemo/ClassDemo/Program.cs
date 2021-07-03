9003f6083d71a266ee347d8b1bf8e96493d8247a
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-07-03 21:30:20 +0300

    added.

---
+++ ClassDemo/ClassDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ÖĞRENCİ BİLGİLERİ SİSTEMİNE HOŞGELDİNİZ :)");
+
+            Student student = new Student(200119037, "Beyza", "Tekinli", 93.4, 92.6, "Anadolu Üniversitesi");
+
+
+            Console.Write("1- Öğrenci Bilgilerini Göster\n2- Öğrenci Ortalamasını Göster\n3- Öğrencinin OKulunu Öğren\n4- Çıkış Yap\nSeçim yapınız: ");
+            int choose = Convert.ToInt32(Console.ReadLine());
+
+            switch (choose)
+            {
+                case 1:
+                    student.showStdInfo();
+                    break;
+                case 2:
+                    Console.WriteLine("Öğrencinin Ortalaması: " + student.findStdAverage());
+                    break;
+                case 3:
+                    student.showSchoolName();
+                    break;
+                case 4:
+                    Console.WriteLine("Çıkış yapıldı.");
+                    break;
+                default:
+                    Console.WriteLine("Hatalı seçim yaptınız lütfen tekrar deneyiniz...");
+                    break;
+            }
+        }
+    }
+}
---
+++ ClassDemo/ClassDemo/Student.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    public class Student
+    {
+
+        // Variables
+        private int stdNo;
+        private string stdName;
+        private string stdSurname;
+        private double stdMidtermExam;
+        private double stdFinalExam;
+        private string stdSchoolName;
+
+
+        // Ctor
+        public Student(int _stdNo, string _stdName, string _stdSurname, double _stdMidtermExam, double _stdFinalExam, string _stdSchoolName)
+        {
+            stdNo = _stdNo;
+            stdName = _stdName;
+            stdSurname = _stdSurname;
+            stdMidtermExam = _stdMidtermExam;
+            stdFinalExam = _stdFinalExam;
+            stdSchoolName = _stdSchoolName;
+        }
+
+
+        // Methods
+        public void showStdInfo()
+        {
+            Console.WriteLine("Numarası: " + stdNo);
+            Console.WriteLine("Adı: " + stdName);
+            Console.WriteLine("Soyadı: " + stdSurname);
+            Console.WriteLine("Vize: " + stdMidtermExam);
+            Console.WriteLine("Final: " + stdFinalExam);
+        }
+
+
+        public double findStdAverage()
+        {
+            double result = (stdMidtermExam * 0.4) + (stdFinalExam * 0.6);
+
+            return result;
+        }
+
+
+        public void showSchoolName()
+        {
+            Console.WriteLine("Okul Adı: " + stdSchoolName);
+        }
+    }
+}
