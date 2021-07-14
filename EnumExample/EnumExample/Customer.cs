fa709e9affdbaaa14aa928a57e289fb071cdb9fb
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-07-15 00:53:41 +0300

    added.

---
+++ EnumExample/EnumExample/Customer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumExample
+{
+    public class Customer
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string gender { get; set; }
+        public string email { get; set; }
+
+
+        public Customer(int id, string name, string surname, string gender, string email)
+        {
+            this.id = id;
+            this.name = name;
+            this.surname = surname;
+            this.gender = gender;
+            this.email = email;
+        }
+
+
+        public static ArrayList customers = new ArrayList();
+
+
+        public CustomerStatus customerAdd(Customer c1)
+        {
+            customers.Add(c1);          // musteri koleksiyona eklendi.
+
+
+            // return 414141;          // değerin eklendiği anlamına gelsin.
+
+            return CustomerStatus.RegistrationSuccessful;
+        }
+    }
+}
---
+++ EnumExample/EnumExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EnumExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer(1, "Berat", "Yılmaz", "Erkek", "beratyilmaz@gmail.com");
+
+            /*
+            customer.id = 1;
+            customer.name = "Berat";
+            customer.surname = "Yılmaz";
+            customer.gender = "Erkek";
+            customer.email = "beratyilmaz@gmail.com";
+            */
+
+
+            // int code = customer.customerAdd(customer);
+
+            /*
+            if (code == 414141)
+            {
+                Console.WriteLine("Müşteri başarılı bir şekilde eklenmiştir.");
+            }
+            */
+
+            CustomerStatus returnStatus = customer.customerAdd(customer);
+
+            if (returnStatus == CustomerStatus.RegistrationSuccessful)
+            {
+                Console.WriteLine("Müşteri başarılı bir şekilde eklenmiştir.");
+            }
+        }
+    }
+}
