b26bee62f469424df1669c5f2e2489a7d78635a7
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-08-11 22:59:43 +0300

    added

---
+++ ToDoApp/ToDoApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToDoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
