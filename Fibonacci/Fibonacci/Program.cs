9e41fc0889c1657487c68a4aabfd99a677c5584f
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-08-17 22:19:14 +0300

    added.

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** FIBONACCI *****");
+
+
+            int a = 0, b = 1, piece, c;
+
+
+            Console.WriteLine("Kaç tane fibonacci sayısı görmek istediğinizi yazınız: ");
+            piece = int.Parse(Console.ReadLine());
+
+            Console.Write(a + " " + b + " ");
+
+            for (int i = 3; i <= piece; i++)
+            {
+                c = a + b;
+
+                Console.Write(c + " ");
+
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
