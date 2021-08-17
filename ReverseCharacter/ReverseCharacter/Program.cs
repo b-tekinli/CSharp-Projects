c23b8f6b9ff4f08eac88f1412dc78fa038aa1c81
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-08-18 01:47:34 +0300

    added.

---
+++ ReverseCharacter/ReverseCharacter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ReverseCharacter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** KARAKTER TERSTEN YAZDIRMA *****");
+
+            Console.Write("Kelime giriniz: ");
+            string word = Console.ReadLine();
+
+            char[] array = word.ToCharArray();
+
+            char temp = array[0];
+
+            char last = array[array.Length - 1];
+
+            array[0] = last;
+            array[array.Length - 1] = temp;
+
+            foreach (var item in array)
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
