5f8d4e6a914d45cbc9a70c7444a5fe75ddb3b17d
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-08-18 00:59:05 +0300

    added.

---
+++ Circle/Circle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** DAİRE ÇİZME *****");
+
+            Console.WriteLine("Dairenin yarı çapını giriniz: ");
+            int r = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= r; i++)
+            {
+                for (int j = 1; j <= r; j++)
+                {
+                    if ((i == 1 || i == r) && (j == 1 || j == r))
+                    {
+                        Console.Write(" ");
+                    }
+                    else if ((i == 1 || i == r) || (j == 1 || j == r))
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+
+                Console.Write("\n");
+            }
+        }
+    }
+}
